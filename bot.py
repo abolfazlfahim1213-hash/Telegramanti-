3066472a3cc5a03e16e524d30cd53ade4bcdf7d2
abolfazlfahim1213-hash <abolfazlfahim1213@gmail.com>
2026-09-05 22:58:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام! 🤖\nربات گپ فعاله 🛡️"
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "/start - شروع ربات\n"
+        "/help - راهنما"
+    )
+
+app = Application.builder().token("TOKEN").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
+
+print("Bot is running...")
+app.run_polling()
